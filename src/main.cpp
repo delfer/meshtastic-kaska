cdd677b362aeef32658e10579b1d9966c94fbaf1
Delfer <bestak47@gmail.com>
2026-02-14 17:53:11 +0300

    Serial1 fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,19 @@
 #define LED_PIN PA15 
 
 void setup() {
-  // Используем Serial1, так как мы включили ENABLE_HWSERIAL1
-  Serial1.setTx(PA9);
-  Serial1.setRx(PA10);
-  Serial1.begin(115200);
+  Serial.setTx(PA9);
+  Serial.setRx(PA10);
+  Serial.begin(115200);
   
   pinMode(LED_PIN, OUTPUT);
-  Serial1.println("Debug output initialized on PA9");
+  Serial.println("Debug output initialized on PA9");
 }
 
 void loop() {
   digitalWrite(LED_PIN, HIGH);
-  Serial1.println("LED ON");
+  Serial.println("LED ON");
   delay(200);
   digitalWrite(LED_PIN, LOW);
-  Serial1.println("LED OFF");
+  Serial.println("LED OFF");
   delay(10000);
 }
