02d7806282973d300e1565e109fd6b9b275e3d63
Delfer <bestak47@gmail.com>
2026-02-14 19:05:40 +0300

    Deep sleep

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <STM32LowPower.h>
 
 // #define ENABLE_I2C_SCANNER
 
@@ -18,6 +19,9 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   Serial.println("Debug output initialized on PA9");
 
+  // Инициализация библиотеки энергосбережения
+  LowPower.begin();
+
 #ifdef ENABLE_I2C_SCANNER
   Wire2.begin();
 #endif
@@ -29,8 +33,25 @@ void loop() {
 #endif
   digitalWrite(LED_PIN, HIGH);
   Serial.println("LED ON");
-  delay(200);
+  delay(200); // while true; do st-flash erase && break ; done
   digitalWrite(LED_PIN, LOW);
   Serial.println("LED OFF");
-  delay(10000);
+
+  // Мгновенное пробуждение обычно вызвано тем, что UART продолжает генерировать прерывания
+  // или линия RX "шумит" при переходе в спящий режим.
+  
+  // 1. Ждем завершения передачи всех данных по Serial
+  Serial.flush();
+  
+  // 2. Выключаем Serial перед сном, чтобы его прерывания не будили МК.
+  // В STM32L0 UART может будить процессор, если это не настроено специально,
+  // но чаще он просто мешает уйти в глубокий сон.
+  // Serial.end();
+
+  // 3. Переводим в deepSleep (режим STOP). В этом режиме SysTick (1мс таймер Arduino) остановлен.
+  // Пробуждение произойдет по внутреннему RTC.
+  LowPower.deepSleep(10000);
+
+  // 4. После пробуждения восстанавливаем Serial для вывода отладки
+  // Serial.begin(115200);
 }
